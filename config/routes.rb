b7b4afb71f319c60e0a4418e1eb899c08e73a0a9
佐藤太一 <3101taichi@gmail.com>
2021-07-12 09:03:50 +0900

    コメント追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
+  # shallow: true
+  # → comment_idを指定しないaction(index, new, create)はpost_idを指定し
+  #   それ以外のactionではcomment_idのみを指定すれば良くなる
+  # e.g.) shallow: false → post_comment  GET  /posts/:post_id/comments/:id(.:format)  comments#show
+  #       shallow: true  → comment       GET  /comments/:id(.:format)                 comments#show
   resources :posts, shallow: true do
     resources :comments
   end
