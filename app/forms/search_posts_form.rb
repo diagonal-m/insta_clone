f517d0ad06a1d7a7901346f463a0dbd54dba1962
佐藤太一 <3101taichi@gmail.com>
2021-07-17 09:15:38 +0900

    複数条件検索を可能に

--- app/forms/search_posts_form.rb
+++ app/forms/search_posts_form.rb
@@ -7,9 +7,10 @@ class SearchPostsForm
   attribute :username, :string
 
   def search
+    # 重複なし全投稿投稿取得
     scope = Post.distinct
     scope = splited_bodies.map { |splited_body| scope.body_contain(splited_body) }.inject { |result, scp| result.or(scp) } if body.present?
-    scope = scope.comment_body_contain(body) if comment_body.present?
+    scope = scope.comment_body_contain(comment_body) if comment_body.present?
     scope = scope.username_contain(username) if username.present?
     scope
   end
@@ -17,6 +18,7 @@ class SearchPostsForm
   private
 
   def splited_bodies
+    # 本文検索窓の文字列を空白区切りの配列として取得
     body.strip.split(/[[:blank:]]+/)
   end
 end
